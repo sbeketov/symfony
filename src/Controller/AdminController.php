7237962e17592e8b9244873b115ad8e26f1b1189
sbeketov <beketov.serg@gmail.com>
2021-10-14 14:19:46 +0300

    prepare to send emails

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Message\CommentMessage;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Workflow\Registry;
+use Twig\Environment;
+
+class AdminController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    public function __construct(
+        private Environment $twig,
+        private EntityManagerInterface $entityManager,
+        private MessageBusInterface $bus,
+    ){}
+
+    #[Route('admin/comment/review/{id}', name: 'review_comment')]
+    public function reviewComment(Request $request, Comment $comment, Registry $registry): Response
+    {
+        $accepted = !$request->query->get('reject');
+
+        $machine = $registry->get($comment);
+        if ($machine->can($comment, 'publish')) {
+            $transition = $accepted ? 'publish' : 'reject';
+        } elseif ($machine->can($comment, 'publish_ham')) {
+            $transition = $accepted ? 'publish_ham' : 'reject_ham';
+        } else {
+            return new Response('Comment already reviewed or not in the right state.');
+        }
+
+        $machine->apply($comment, $transition);
+        $this->entityManager->flush();
+
+        if ($accepted) {
+            $this->bus->dispatch(new CommentMessage($comment->getId()));
+        }
+
+        return $this->render('admin/review.html.twig', [
+            'transition' => $transition,
+            'comment' => $comment,
+        ]);
+    }
+}
--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -7,6 +7,8 @@ use App\Repository\CommentRepository;
 use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
@@ -20,6 +22,8 @@ class CommentMessageHandler implements MessageHandlerInterface
         private SpamChecker $spamChecker,
         private CommentRepository $commentRepository,
         private MessageBusInterface $bus,
+        private MailerInterface $mailer,
+        private string $adminEmail,
         WorkflowInterface $commentStateMachine,
         private LoggerInterface|null $logger = null,
     ){
@@ -47,8 +51,13 @@ class CommentMessageHandler implements MessageHandlerInterface
 
             $this->bus->dispatch($message);
         } elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
-            $this->workflow->apply($comment, $this->workflow->can($comment, 'publish') ? 'publish' : 'publish_ham');
-            $this->entityManager->flush();
+            $this->mailer->send((new NotificationEmail())
+                        ->subject('New comment posted')
+                        ->htmlTemplate('emails/comment_notification.html.twig')
+                        ->to($this->adminEmail)
+                        ->context(['comment' => $comment])
+            );
+
         } elseif ($this->logger) {
             $this->logger->debug('Dropping comment message', ['comment' => $comment->getId(), 'state' => $comment->getState()]);
         }
